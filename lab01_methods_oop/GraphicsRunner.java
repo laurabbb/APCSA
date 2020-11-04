7c67114c37cd988a5af8f27edf1071549811af5e
Laura Barnes <52331374+laurabbb@users.noreply.github.com>
2020-11-03 23:50:40 -0600

    Add files via upload

---
+++ lab01_methods_oop/GraphicsRunner.java
@@ -0,0 +1,36 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Laura Barnes
+//Class - AP CSA
+//Date - September 17th, 2019
+//Lab  - Smiley Face, Robot, and Big House
+
+import javax.swing.JFrame;
+
+public class GraphicsRunner extends JFrame
+{
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+
+	public GraphicsRunner()
+	{
+		super("Graphics Runner");
+
+		setSize(WIDTH,HEIGHT);
+
+		getContentPane().add(new ShapePanel());
+		
+		//add other classes to run them 
+		//BigHouse, Robot, or ShapePanel 
+
+		setVisible(true);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main( String args[] )
+	{
+		GraphicsRunner run = new GraphicsRunner();
+	}
+}
